488d38e68fa6e645243bd6fde8a2f66725c5b4f3
Jayso <sdi1300085@di.uoa.gr>
2019-10-23 18:16:45 +0300

    weghh

---
+++ Hypercube_vertices.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "structs.h"
+
+Hypercube_vertices::Hypercube_vertices(){}
+
+Hypercube_vertices::~Hypercube_vertices(){}
+
+void Hypercube_vertices::set_code(string HCcode){
+    code = HCcode;
+}
+
+int Hypercube_vertices::get_code(){
+    return code;
+}
+
+int* Hypercube_vertices::get_p_vector(){
+    return p_vector;
+}
--- structs.h
+++ structs.h
@@ -17,3 +17,17 @@ class Vector_Item{
         vector<double> get_vector();
         void print_item();
 };
+
+
+class Hypercube_vertices{
+    private:
+        string code;
+        int *p_vector;
+
+    public:
+        Hypercube_vertices();
+        ~Hypercube_vertices();
+        void set_code(string HCcode)
+        string get_code();
+        int* get_p_vector();
+}
