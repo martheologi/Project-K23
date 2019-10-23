7b3f301ae7cf18f74ede266fa6079f89ffbec162
martheologi <martheo1708@gmail.com>
2019-10-23 18:02:30 +0300

    done with lsh

--- funct.cpp
+++ funct.cpp
@@ -56,14 +56,13 @@ int Initialize_Dataset_Vector(string filename, vector<Vector_Item>* Items){
         //apo8hkeuw to dataset se ena vector
         Items->push_back(item);
         id_flag = 1;
-        //item.print_item();
     }
     file.close();
 
     return c;
 }
 
-Vector_Item ExactNN(vector<Vector_Item> Items, Vector_Item item, int c){
+Vector_Item ExactNN(vector<Vector_Item> Items, Vector_Item item, int c, int* ExactNN_dist){
     int NN_pos = -1;
     int d = item.get_vector().size();
     int NN_dist;
@@ -80,11 +79,12 @@ Vector_Item ExactNN(vector<Vector_Item> Items, Vector_Item item, int c){
             min=NN_dist;
         }
     }
-    cout << Items.at(NN_pos).get_item_id() << " " << NN_dist << endl;
+
+    *ExactNN_dist = min;
     return Items.at(NN_pos);
 }
 
-Vector_Item AproximateNN(vector<Vector_Item> Items, Vector_Item item, vector<unordered_multimap<int, int>> HT, int buckets, int k, int L, long int m, int M, int W){
+Vector_Item AproximateNN(vector<Vector_Item> Items, Vector_Item item, vector<unordered_multimap<int, int>> HT, int buckets, int k, int L, long int m, int M, int W, int* AprNN_dist){
     int NN_id = -1;
     int NN_position = -1;
     long int NN_dist = 10000000000000;
@@ -94,7 +94,6 @@ Vector_Item AproximateNN(vector<Vector_Item> Items, Vector_Item item, vector<uno
     for(int l=0; l<L; l++){
         //ftiaxnw k hash sunarthseis
         int key = hash_key(item, buckets, d, k, L, W, M, m);//g%buckets;
-        //cout << "key of query " << item.get_item_id() << " is " << key << endl;
         auto found = HT.at(l).find(key);
 
         if(found != HT.at(l).end()){
@@ -105,7 +104,6 @@ Vector_Item AproximateNN(vector<Vector_Item> Items, Vector_Item item, vector<uno
             for (auto it = itr.first; it != itr.second; it++) {
                 retrieved_items++;
                 Vector_Item neighbor_item = Items.at(it->second); //edw 8a xreiastei mia find an to id den einai 0-n
-                //cout << neighbor_item.get_item_id() << "\t";
 
                 if(retrieved_items > 3*L) break;
 
@@ -115,21 +113,17 @@ Vector_Item AproximateNN(vector<Vector_Item> Items, Vector_Item item, vector<uno
                    NN_dist = distance_l1(item.get_vector(), neighbor_item.get_vector(), d);
                 }
             }
-            //cout << "Possiboool NN of query " << item.get_item_id() << " is with dist = " << NN_dist << endl;
-            //Items.at(NN_position).print_item();
         }
         else
             cout << "!!!!!!!!!!!there is no key " << key << endl;
-        //cout << endl;
     }
-    cout << "FINAL NN of query " << item.get_item_id() << " is " << Items.at(NN_position).get_item_id() << " " << NN_dist << endl;
-    //Items.at(NN_position).print_item();
+    *AprNN_dist = NN_dist;
 
     return Items.at(NN_position);
 }
 
 //grafei ta apotelesmata sto arxeio output
-void write_results(string filename, int qID, int NnID, double tLSH, double tTrue)
+void write_results(string filename, int qID, int NnID, int dLSH, int dTrue, double tLSH, double tTrue)
 {
     ofstream file;
     file.open (filename, ios::app); // append mode
@@ -137,8 +131,8 @@ void write_results(string filename, int qID, int NnID, double tLSH, double tTrue
     if(file.is_open()){
         file << "Query:" << qID << endl;
         file << "Nearest neighbor:" << NnID << endl;
-      //  file << "distanceLSH:" << dLSH << endl;
-      //  file << "distanceTrue:" << dTrue << endl;
+        file << "distanceLSH:" << dLSH << endl;
+        file << "distanceTrue:" << dTrue << endl;
         file << "tLSH:" << tLSH << " s." << endl;
         file << "tTrue:" << tTrue << " s." << endl << endl;
     }
