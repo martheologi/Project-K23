c4ab06c8414d3f1673a4ae5ab88e9dbe4376703e
martheologi <martheo1708@gmail.com>
2019-10-25 15:51:32 +0300

    under construction

---
+++ Vector_LSH_Hypercube/structs.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Vector_Item{
+    private:
+        int item_id;
+        vector<double> x;
+
+    public:
+        Vector_Item();
+        ~Vector_Item();
+        void set_id(int id);
+        void push(double xi);
+        int get_item_id();
+        vector<double> get_vector();
+        void print_item();
+};
+
+
+class Hypercube_vertices{
+    private:
+        string code;
+        vector<int> p_vector;
+
+    public:
+        Hypercube_vertices();
+        ~Hypercube_vertices();
+        void set_code(string HCcode);
+        string get_code();
+        vector<int> get_p_vector();
+        void add_point(int point_pos);
+};
+
+int find_vertice(vector<Hypercube_vertices> HyperCube, string vertice);
+
+void print_HyperCube(vector<Hypercube_vertices> HyperCube);
